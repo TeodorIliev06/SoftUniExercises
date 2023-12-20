dfa629ada10f20770a9b9758acad8b8b8b9c8a57
TeodorIliev06 <tedo2505@gmail.com>
2023-12-20 20:21:52 +0200

    Added files
    Added some solutions for if-else exercises

---
+++ CSharp Basics/October '23/Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double S = a * a;
+                Console.WriteLine($"{S:f3}");
+            }
+            if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double S = a * b;
+                Console.WriteLine($"{S:f3}");
+            }
+            if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                double S = r * r * Math.PI;
+                Console.WriteLine($"{S:f3}");
+            }
+            if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                double S = a * h / 2;
+                Console.WriteLine($"{S:f3}");
+            }
+        }
+    }
+}

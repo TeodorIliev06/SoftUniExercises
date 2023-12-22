6e638fab799bcff70025030da3de57aadba74857
TeodorIliev06 <tedo2505@gmail.com>
2023-12-22 16:30:11 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statements - Exercise/04. Toy Shop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine());
+            int puzzle = int.Parse(Console.ReadLine());
+            int doll = int.Parse(Console.ReadLine());
+            int bear = int.Parse(Console.ReadLine());
+            int minion = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+
+            double puzzlePrice = 2.60;
+            double dollPrice = 3.0;
+            double bearPrice = 4.10;
+            double minionPrice = 8.20;
+            double truckPrice = 2.0;
+
+            int count = puzzle + doll + bear + minion + truck;
+            double totalPrice = puzzlePrice * puzzle + bearPrice * bear 
+                + dollPrice * doll + minionPrice * minion + truckPrice * truck;
+
+            if (count >= 50) 
+            { 
+                price *= 0.75; 
+            }
+
+            price *= 0.9;
+
+            if (price >= price) 
+            { 
+                Console.WriteLine($"Yes! {price - price:F2} lv left."); 
+            }
+            else 
+            { 
+                Console.WriteLine($"Not enough money! {price - price:F2} lv needed.");
+            }
+        }
+    }
+}
---
+++ CSharp Programming Basics/October '23/Conditional Statements - Exercise/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.1;
+
+            if (statists > 150) 
+            {
+                price = price * 0.9; 
+            }
+
+            double total = decor + (price * statists);
+
+            if (total > budget) 
+            {
+                Console.WriteLine("Not enough money!"); 
+                Console.WriteLine($"Wingard needs {(total - budget):F2} leva more."); 
+            }
+            else if (total <= budget) 
+            {
+                Console.WriteLine("Action!"); 
+                Console.WriteLine($"Wingard starts filming with {(budget - total):F2} leva left.");
+            }
+        }
+    }
+}

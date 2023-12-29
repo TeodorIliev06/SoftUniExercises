87aab7824df4d119df7732e1a193619057b84851
TeodorIliev06 <tedo2505@gmail.com>
2023-12-29 23:54:04 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statements - Exercise/07. Shopping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int graphicCards = int.Parse(Console.ReadLine());
+            int cpu = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+            double priceGraphicCards = graphicCards * 250;
+            double priceCpu = (priceGraphicCards * 0.35) * cpu;
+            double priceRam = (priceGraphicCards * 0.10) * ram;
+            double totalPrice = priceGraphicCards + priceCpu + priceRam;
+
+            if (graphicCards > cpu) 
+            { 
+                totalPrice *= 0.85;
+            }
+            if (budget >= totalPrice) 
+            { 
+                Console.WriteLine($"You have {budget - totalPrice:F2} leva left!"); 
+            }
+            else 
+            { 
+                Console.WriteLine($"Not enough money! You need {totalPrice - budget:F2} leva more!"); 
+            }
+        }
+    }
+}

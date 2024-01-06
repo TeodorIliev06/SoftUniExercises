0a489f3b69b610236c66bd68fca2951c9ddd0e58
TeodorIliev06 <tedo2505@gmail.com>
2024-01-06 17:05:41 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statement Advanced - Lab/09. Fruit or Vegetable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "banana", "apple", "kiwi", "cherry", "lemon", "grapes" };
+            string[] vegetables = { "tomato", "cucumber", "pepper", "carrot" };
+            string input = Console.ReadLine();
+            string category = "unknown";
+            foreach (string fruit in fruits)
+            {
+                if (input == fruit) { category = "fruit"; break; }
+            }
+            foreach (string vegetable in vegetables)
+            {
+                if (input == vegetable) { category = "vegetable"; break; }
+            }
+            Console.WriteLine(category);
+        }
+    }
+}

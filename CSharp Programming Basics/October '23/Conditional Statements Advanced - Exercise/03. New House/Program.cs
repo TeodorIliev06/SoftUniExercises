17f87a234146394bcc515c812941213c822f0f65
TeodorIliev06 <tedo2505@gmail.com>
2024-01-07 23:04:36 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statements Advanced - Exercise/03. New House/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string flower = Console.ReadLine();
+            int ctr = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (flower == "Roses")
+            {
+                price = ctr * 5;
+                if (ctr > 80) { price *= 0.9; }
+            }
+            else if (flower == "Dahlias")
+            {
+                price = ctr * 3.80;
+                if (ctr > 90) { price *= 0.85; }
+            }
+            else if (flower == "Tulips")
+            {
+                price = ctr * 2.80;
+                if (ctr > 80) { price *= 0.85; }
+            }
+            else if (flower == "Narcissus")
+            {
+                price = ctr * 3;
+                if (ctr < 120)
+                {
+                    price *= 1.15;
+                }
+            }
+            else if (flower == "Gladiolus")
+            {
+                price = ctr * 2.50;
+                if (ctr < 80) { price *= 1.2; }
+            }
+            if (price <= budget) { Console.WriteLine($"Hey, you have a great garden with {ctr} {flower} and {(budget - price):F2} leva left."); }
+            else if (price > budget) { Console.WriteLine($"Not enough money, you need {(price - budget):F2} leva more."); }
+        }
+    }
+}

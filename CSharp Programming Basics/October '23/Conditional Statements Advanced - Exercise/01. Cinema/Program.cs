ae48b5dc5413cd8b7595c27b2c1ca010408d8718
TeodorIliev06 <tedo2505@gmail.com>
2024-01-07 22:48:31 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (projection == "Premiere") { price = 12.00 * rows * cols; }
+            else if (projection == "Normal") { price = 7.50 * rows * cols; }
+            else if (projection == "Discount") { price = 5.00 * rows * cols; }
+            Console.WriteLine($"{price:F2} leva");
+        }
+    }
+}

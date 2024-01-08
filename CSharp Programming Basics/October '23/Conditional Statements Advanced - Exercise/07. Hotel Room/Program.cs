a9f066f33b7f3acd03c8570bc098eb35bd3a314f
TeodorIliev06 <tedo2505@gmail.com>
2024-01-08 22:38:54 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statements Advanced - Exercise/07. Hotel Room/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int ctr = int.Parse(Console.ReadLine());
+            double priceStudio = 0, priceApartment = 0;
+            if (month == "May" || month == "October")
+            {
+                priceStudio = 50 * ctr;
+                priceApartment = 65 * ctr;
+                if (ctr > 7 && ctr < 14) { priceStudio *= 0.95; }
+                else if (ctr > 14) { priceStudio *= 0.70; priceApartment *= 0.90; }
+                Console.WriteLine($"Apartment: {priceApartment:F2} lv.");
+                Console.WriteLine($"Studio: {priceStudio:F2} lv.");
+            }
+            else if (month == "June" || month == "September")
+            {
+                priceStudio = 75.20 * ctr;
+                priceApartment = 68.70 * ctr;
+                if (ctr > 14) { priceStudio *= 0.80; priceApartment *= 0.90; }
+                Console.WriteLine($"Apartment: {priceApartment:F2} lv.");
+                Console.WriteLine($"Studio: {priceStudio:F2} lv.");
+            }
+            else if (month == "July" || month == "August")
+            {
+                priceStudio = 76 * ctr;
+                priceApartment = 77 * ctr;
+                if (ctr > 14) { priceApartment *= 0.90; }
+                Console.WriteLine($"Apartment: {priceApartment:F2} lv.");
+                Console.WriteLine($"Studio: {priceStudio:F2} lv.");
+            }
+        }
+    }
+}

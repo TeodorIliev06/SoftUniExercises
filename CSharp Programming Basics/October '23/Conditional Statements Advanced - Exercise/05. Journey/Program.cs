bdf4ccdfc2a522e8d015f2843a9b18d93f62d8e2
TeodorIliev06 <tedo2505@gmail.com>
2024-01-08 22:31:35 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/Conditional Statements Advanced - Exercise/05. Journey/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string seazon = Console.ReadLine();
+            string place = string.Empty;
+            double sum = 0;
+            if (budget <= 100 && seazon == "summer") { sum = budget * 0.3; Console.WriteLine("Somewhere in Bulgaria"); Console.WriteLine($"Camp - {sum:F2}"); }
+            else if (budget <= 100 && seazon == "winter") { sum = budget * 0.7; Console.WriteLine("Somewhere in Bulgaria"); Console.WriteLine($"Hotel - {sum:F2}"); }
+            else if (budget > 100 && budget <= 1000 && seazon == "summer") { sum = budget * 0.4; Console.WriteLine("Somewhere in Balkans"); Console.WriteLine($"Camp - {sum:F2}"); }
+            else if (budget > 100 && budget <= 1000 && seazon == "winter") { sum = budget * 0.8; Console.WriteLine("Somewhere in Balkans"); Console.WriteLine($"Hotel - {sum:F2}"); }
+            else if (budget > 1000) { sum = budget * 0.9; Console.WriteLine("Somewhere in Europe"); Console.WriteLine($"Hotel - {sum:F2}"); }
+        }
+    }
+}
---
+++ CSharp Programming Basics/October '23/Conditional Statements Advanced - Exercise/06. Operations Between Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double sum = 0.0;
+            string operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                sum = a + b;
+                if (sum % 2 == 0) { Console.WriteLine($"{a} + {b} = {sum} - even"); }
+                else { Console.WriteLine($"{a} + {b} = {sum} - odd"); }
+            }
+            if (operation == "-")
+            {
+                sum = a - b;
+                if (sum % 2 == 0) { Console.WriteLine($"{a} - {b} = {sum} - even"); }
+                else { Console.WriteLine($"{a} - {b} = {sum} - odd"); }
+            }
+            if (operation == "*")
+            {
+                sum = a * b;
+                if (sum % 2 == 0) { Console.WriteLine($"{a} * {b} = {sum} - even"); }
+                else { Console.WriteLine($"{a} * {b} = {sum} - odd"); }
+            }
+            if (operation == "/")
+            {
+                if (b == 0) { Console.WriteLine($"Cannot divide {a} by zero"); }
+                else if (b != 0)
+                {
+                    sum = (double)a / (double)b;
+                    Console.WriteLine($"{a} / {b} = {sum:F2}");
+                }
+            }
+            if (operation == "%")
+            {
+                if (b == 0) { Console.WriteLine($"Cannot divide {a} by zero"); }
+                else if (b != 0)
+                {
+                    sum = (double)a % (double)b;
+                    Console.WriteLine($"{a} % {b} = {sum}");
+                }
+            }
+        }
+    }
+}

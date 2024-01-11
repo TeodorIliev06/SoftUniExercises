702fbc030d7b24efa776b70b1f9bad44cdf742f2
TeodorIliev06 <tedo2505@gmail.com>
2024-01-11 21:55:43 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char letter = input[i];
+            Console.WriteLine(letter);
+        }
+    }
+}

347caffbdf57ec26dab9962045d4bc80c2e97cfb
TeodorIliev06 <tedo2505@gmail.com>
2024-01-12 21:40:48 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int max = -1000000;
+        int min = 1000000;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (max < num) { max = num; }
+            if (num < min) { min = num; }
+        }
+        Console.WriteLine($"Max number: {max}");
+        Console.WriteLine($"Min number: {min}");
+    }
+}

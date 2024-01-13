0036592bf2b6140c57115c493f74ba973aa205fa
TeodorIliev06 <tedo2505@gmail.com>
2024-01-13 11:50:55 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0, max = -10000;
+        for (int i = 1; i <= n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num > max)
+            {
+                max = num;
+            }
+            sum += num;
+        }
+        if (max == (sum - max)) { Console.WriteLine("Yes"); Console.WriteLine($"Sum = {max}"); }
+        else
+        {
+            int diff = Math.Abs(max - (sum - max));
+            Console.WriteLine("No");
+            Console.WriteLine($"Diff = {diff}");
+        }
+    }
+}

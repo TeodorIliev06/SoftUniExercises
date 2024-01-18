9e06afeea94b7d79025f11d413c0ae53dd20e9ea
TeodorIliev06 <tedo2505@gmail.com>
2024-01-18 23:07:26 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double min = 10000000;
+            string number = Console.ReadLine();
+
+            while (number != "Stop")
+            {
+                if (double.Parse(number) < min)
+                {
+                    min = double.Parse(number);
+                }
+                number = Console.ReadLine();
+            }
+            Console.WriteLine(min);
+        }
+    }
+}

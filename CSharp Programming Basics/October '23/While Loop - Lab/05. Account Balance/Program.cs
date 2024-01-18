6d3e73819927b31b1e0de4426d61ec5c9ba70a3c
TeodorIliev06 <tedo2505@gmail.com>
2024-01-18 22:59:00 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vnoska = Console.ReadLine();
+            double sum = 0;
+            while (vnoska != "NoMoreMoney")
+            {
+                if (double.Parse(vnoska) < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {double.Parse(vnoska):F2}");
+                sum += double.Parse(vnoska);
+                vnoska = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:F2}");
+        }
+    }
+}
---
+++ CSharp Programming Basics/October '23/While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double max = -10000000;
+            string number = Console.ReadLine();
+
+            while (number != "Stop")
+            {
+                if (double.Parse(number) > max)
+                {
+                    max = double.Parse(number);
+                }
+                number = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+    }
+}

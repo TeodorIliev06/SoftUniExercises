4c6393da855a909fd89747b4e3d5a9c34a50527d
TeodorIliev06 <tedo2505@gmail.com>
2024-01-20 22:40:01 +0200

    Added files

---
+++ CSharp Programming Basics/October '23/While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int br = 0;
+            string book;
+
+            while ((book = Console.ReadLine()) != "No More Books")
+            {
+                if (book == name)
+                {
+                    Console.WriteLine($"You checked {br} books and found it.");
+                    break;
+                }
+                br++;
+                book = Console.ReadLine();
+            }
+            if (book != name)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {br} books.");
+            }
+        }
+    }
+}

d1e92fecfe52b1b35b3250c3894d4c6684c4c9d9
TeodorIliev06 <tedo2505@gmail.com>
2024-01-23 21:54:04 +0200

    Added files

---
+++ CSharp Programming Basics/November '23/Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string mqsto = Console.ReadLine();
+                if (mqsto == "End") { break; }
+                double min = double.Parse(Console.ReadLine());
+
+                double saved = 0;
+
+                while (saved < min)
+                {
+                    double currentSaved = double.Parse(Console.ReadLine());
+
+                    saved += currentSaved;
+                }
+
+                Console.WriteLine($"Going to {mqsto}!");
+            }
+        }
+    }
+}

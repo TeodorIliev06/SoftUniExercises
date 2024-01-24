905063bb3bdfeec55e07a4ce9dca3c4c4a65beee
TeodorIliev06 <tedo2505@gmail.com>
2024-01-24 22:11:44 +0200

    Added files

---
+++ CSharp Programming Basics/November '23/Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int judges = int.Parse(Console.ReadLine());
+            string input, prezentation;
+            int ctr = 0;
+            double sum = 0;
+
+            while ((input = Console.ReadLine()) != "Finish")
+            {
+                double prezentationEV = 0;
+                prezentation = input;
+                for (int i = 1; i <= judges; i++)
+                {
+                    prezentationEV += double.Parse(Console.ReadLine());
+                }
+                prezentationEV = prezentationEV / judges;
+                sum += prezentationEV;
+
+                Console.WriteLine($"{prezentation} - {prezentationEV:F2}.");
+                ctr++;                
+            }
+
+            Console.WriteLine($"Student's final assessment is {sum / ctr:F2}.");
+        }
+    }
+}
---
+++ CSharp Programming Basics/November '23/Nested Loops - Exercise/06. Cinema Tickets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CinemaTickets
+{
+    class Program
+    {
+        static void Main()
+        {
+            int standardTicket = 0;
+            int studentTicket = 0;
+            int kidTicket = 0;
+            int totalSeats = 0;
+            string movie;
+
+            while ((movie = Console.ReadLine()) != "Finish")
+            {
+                int allSeats = int.Parse(Console.ReadLine());
+                int takenSeats = 0;
+                string ticketType;
+
+                while (takenSeats < allSeats && (ticketType = Console.ReadLine()) != "End")
+                {
+                    switch (ticketType)
+                    {
+                        case "standard":
+                            standardTicket++;
+                            break;
+                        case "student":
+                            studentTicket++;
+                            break;
+                        case "kid":
+                            kidTicket++;
+                            break;
+                    }
+                    takenSeats++;
+                }
+
+                double utilized = (double)takenSeats / (double)allSeats * 100;
+                Console.WriteLine($"{movie} - {utilized:F2}% full.");
+                totalSeats += takenSeats;
+            }
+
+            Console.WriteLine($"Total tickets: {totalSeats}");
+            Console.WriteLine($"{((decimal)studentTicket / totalSeats * 100):F2}% student tickets.");
+            Console.WriteLine($"{((decimal)standardTicket / totalSeats * 100):F2}% standard tickets.");
+            Console.WriteLine($"{((decimal)kidTicket / totalSeats * 100):F2}% kids tickets.");
+        }
+    }
+}

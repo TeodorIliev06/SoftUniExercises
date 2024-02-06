6a05681e318c630a9051326d68f8ab2063e2be86
TeodorIliev06 <tedo2505@gmail.com>
2024-02-06 18:49:17 +0200

    Added files

---
+++ CSharp Programming Fundamemtals/Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and sum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04._Print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            for (int i = first; i <= second; i++)
+            {
+                sum += i;
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}

e757e35ba255c4c880ec4584e384d473c6d3aa3c
TeodorIliev06 <tedo2505@gmail.com>
2024-02-10 22:18:22 +0200

    Added files

---
+++ CSharp Programming Fundamemtals/Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string reverseWord = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseWord += word[i];
+            }
+            Console.WriteLine(reverseWord);
+        }
+    }
+}

b05c7716a4c83aea4272554f3b5aba975d894278
TeodorIliev06 <tedo2505@gmail.com>
2024-02-14 23:35:01 +0200

    Added files

---
+++ CSharp Programming Fundamemtals/Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meter = int.Parse(Console.ReadLine());
+            double km = (double)meter / 1000;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
---
+++ CSharp Programming Fundamemtals/Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            double dollars = pounds * 1.31;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}

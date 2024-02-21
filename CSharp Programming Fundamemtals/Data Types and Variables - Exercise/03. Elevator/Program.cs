90658537029cf0a1f02109a632c2b29ba78e5fa7
TeodorIliev06 <tedo2505@gmail.com>
2024-02-21 23:00:57 +0200

    Updated Data Types and Variables - Exercise

---
+++ CSharp Programming Fundamemtals/Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = Convert.ToInt32(Console.ReadLine());
+            int capacity = Convert.ToInt32(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)people / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}

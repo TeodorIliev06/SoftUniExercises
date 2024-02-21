a4f93c1a51f8a9caffc5d92584e34dfd5070f832
TeodorIliev06 <tedo2505@gmail.com>
2024-02-21 23:48:09 +0200

    Updated Data Types and Variables - Exercise

---
+++ CSharp Programming Fundamemtals/Data Types and Variables - Exercise/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstRange = int.Parse(Console.ReadLine());
+            int secondRange = int.Parse(Console.ReadLine());
+
+            for (int i = firstRange; i <= secondRange; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}

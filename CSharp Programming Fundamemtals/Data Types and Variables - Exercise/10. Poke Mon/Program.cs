0407c5de6d315b117e845ce3a9652c0c7ee0b0b7
TeodorIliev06 <tedo2505@gmail.com>
2024-02-22 18:16:25 +0200

    Updated Data Types and Variables - Exercise

---
+++ CSharp Programming Fundamemtals/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int pokePowerCurrent = pokePower;
+            int distanceBetwTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int hitTargets = 0;
+
+            do
+            {
+                pokePowerCurrent -= distanceBetwTargets;
+                hitTargets++;
+
+                if (pokePowerCurrent == pokePower / 2.0 && exhaustionFactor != 0)
+                {
+                    pokePowerCurrent = pokePowerCurrent / exhaustionFactor;
+                }
+
+            } while (pokePowerCurrent >= distanceBetwTargets);
+
+            Console.WriteLine(pokePowerCurrent);
+            Console.WriteLine(hitTargets);
+        }
+    }
+}

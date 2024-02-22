7698e1147cb7cad8b2c13a3440f84157895a4191
TeodorIliev06 <tedo2505@gmail.com>
2024-02-22 18:11:23 +0200

    Updated Data Types and Variables - Exercise

---
+++ CSharp Programming Fundamemtals/Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int capacity = 255;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int add = int.Parse(Console.ReadLine());
+
+                if (sum + add > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    sum += add;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

e0076fbf5226dc830ce4ad38f26a38d53cbe3a46
TeodorIliev06 <tedo2505@gmail.com>
2024-02-26 20:25:39 +0200

    Added files to Array - Lab

---
+++ CSharp Programming Fundamemtals/Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int output = 0;
+            int numbersLength = numbers.Length - 1;
+            if (numbersLength == 0)
+            {
+                Console.WriteLine(numbers[0]);
+                return;
+            }
+            for (int i = 0; i < numbersLength; i++)
+            {
+                int[] condensed = new int[numbersLength];
+                for (int j = 0; j < condensed.Length; j++)
+                    condensed[j] = numbers[j] + numbers[j + 1];
+                numbers = condensed;
+                output = condensed[0];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}

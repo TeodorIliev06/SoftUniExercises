b91c5c979a4bf2a827caa448d18fa4bc302e0469
TeodorIliev06 <tedo2505@gmail.com>
2024-02-26 18:45:58 +0200

    Created Arrays - Lab

---
+++ CSharp Programming Fundamemtals/Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                double[] array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+                foreach (double number in array)
+                {
+                    int rounded = (int)Math.Round(number, MidpointRounding.AwayFromZero);
+                    Console.WriteLine("{0} => {1}", number, rounded);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp Programming Fundamemtals/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}

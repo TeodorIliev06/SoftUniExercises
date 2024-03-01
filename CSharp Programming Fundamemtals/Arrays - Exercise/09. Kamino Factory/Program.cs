0e3a28ee4059b2bc0c8267bdd4545da819a9691b
TeodorIliev06 <tedo2505@gmail.com>
2024-03-01 17:06:46 +0200

    Added files to Arrays - Exercise

---
+++ CSharp Programming Fundamemtals/Arrays - Exercise/09. Kamino Factory/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine(); // Skipping the first line
+
+            string[] bestDna = null;
+            int bestLength = -1;
+            int bestStartIndex = -1;
+            int bestDnaSum = 0;
+            int bestSampleIndex = 0;
+
+            int currentSampleIndex = 0;
+
+            string input;
+            while ((input = Console.ReadLine()) != "Clone them!")
+            {
+                string[] currentDna = input.Split('!', StringSplitOptions.RemoveEmptyEntries);
+
+                int currentLength = 0;
+                int currentBestLength = 0;
+                int currentEndIndex = 0;
+
+                for (int i = 0; i < currentDna.Length; i++)
+                {
+                    if (currentDna[i] == "1")
+                    {
+                        currentLength++;
+                        if (currentLength > currentBestLength)
+                        {
+                            currentEndIndex = i;
+                            currentBestLength = currentLength;
+                        }
+                    }
+                    else
+                    {
+                        currentLength = 0;
+                    }
+                }
+
+                int currentStartIndex = currentEndIndex - currentBestLength + 1;
+
+                int currentDnaSum = currentDna.Select(int.Parse).Sum();
+
+                bool isCurrentDnaBetter = currentBestLength > bestLength ||
+                                           (currentBestLength == bestLength && currentStartIndex < bestStartIndex) ||
+                                           (currentBestLength == bestLength && currentStartIndex == bestStartIndex && currentDnaSum > bestDnaSum);
+
+                currentSampleIndex++;
+
+                if (isCurrentDnaBetter)
+                {
+                    bestDna = currentDna;
+                    bestLength = currentBestLength;
+                    bestStartIndex = currentStartIndex;
+                    bestDnaSum = currentDnaSum;
+                    bestSampleIndex = currentSampleIndex;
+                }
+            }
+
+            Console.WriteLine($"Best DNA sample {bestSampleIndex} with sum: {bestDnaSum}.");
+            Console.WriteLine(string.Join(' ', bestDna));
+        }
+    }
+}

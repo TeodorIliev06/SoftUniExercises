037285024c81f388b21ffee93394581f3f5f94a9
TeodorIliev06 <tedo2505@gmail.com>
2024-03-03 23:22:07 +0200

    Updated - More Exercise

---
+++ CSharp Programming Fundamemtals/Arrays - More Exercise/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03._Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(FibonachiRecursion(n));
+        }
+        static long FibonachiRecursion(int n)
+        {
+            if (n <= 2) //създаваме дъно на рекурсията
+            {
+                return 1;
+            }
+            return FibonachiRecursion(n - 1) + FibonachiRecursion(n - 2);
+        }
+    }
+}
---
+++ CSharp Programming Fundamemtals/Arrays - More Exercise/04. Fold and Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Fold_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int foldPoint = originalArray.Length / 4;
+            int[] output = new int[foldPoint * 2];
+
+            for (int i = 0; i < foldPoint; i++)
+            {
+                output[i] = originalArray[i + foldPoint] + originalArray[foldPoint - 1 - i];
+                //Console.WriteLine(output[i]);
+
+                output[i + foldPoint] = originalArray[i + 2 * foldPoint] + originalArray[originalArray.Length - 1 - i];
+                //Console.WriteLine(output[i+foldPoint]);
+            }
+
+            Console.WriteLine(String.Join(' ', output));
+        }
+    }
+}

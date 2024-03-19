1f2c3dceaf8c68b02e6069888e29ef801be6c701
TeodorIliev06 <tedo2505@gmail.com>
2024-03-19 19:19:08 +0200

    Create Methods - More Exercise

---
+++ CSharp Programming Fundamemtals/Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string command = Console.ReadLine();
+            Convert(type, command);
+        }
+
+        private static void Convert(string type, string command)
+        {
+            if (type == "int")
+            {
+                int num = int.Parse(command) * 2;
+                Console.WriteLine(num);
+            }
+            if (type == "real")
+            {
+                double num = double.Parse(command) * 1.5;
+                Console.WriteLine($"{num:f2}");
+            }
+            if (type == "string")
+            {
+                Console.WriteLine($"${command}$");
+            }
+        }
+    }
+}

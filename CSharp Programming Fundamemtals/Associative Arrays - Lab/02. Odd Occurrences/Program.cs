ea2c5d56c420a45260d397aacc9433d265268c99
TeodorIliev06 <tedo2505@gmail.com>
2024-03-27 23:23:11 +0200

    Created Associative Arrays - Lab

---
+++ CSharp Programming Fundamemtals/Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split()
+                .Select(x => x.ToLower())
+                .ToArray();
+
+            Dictionary<string, int> wordOccurences = new Dictionary<string, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!wordOccurences.ContainsKey(numbers[i]))
+                {
+                    wordOccurences[numbers[i]] = 0;
+                }
+                wordOccurences[numbers[i]]++;
+            }
+
+            foreach (KeyValuePair<string, int> itemPair in wordOccurences)
+            {
+                if (itemPair.Value % 2 == 1)
+                {
+                    Console.Write(itemPair.Key + " ");
+                }
+            }
+        }
+    }
+}

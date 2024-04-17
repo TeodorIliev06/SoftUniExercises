0a8fa3ee4ebb43b9ce7ad6898b8428fe5e73bdfc
TeodorIliev06 <tedo2505@gmail.com>
2024-04-17 22:21:23 +0300

    Added Regular Expressions - Lab

---
+++ CSharp Programming Fundamemtals/Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+
+            Console.WriteLine();
+        }
+        /*Bethany Taylor, Oliver miller, sophia Johnson, SARah Wilson, John Smith, Sam  Smith
+          */
+    }
+}

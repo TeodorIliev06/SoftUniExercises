56c66ed7232fba8e9ac419a8f5fbe5839f372856
TeodorIliev06 <tedo2505@gmail.com>
2024-06-06 23:31:47 +0300

    Created Multidimensional Arrays - Exercise

---
+++ C# Advanced/Multidimensional Arrays - Exercise/DiagonalDifference/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+            int firstDiagonalSum = 0;
+            int secondDiagonalSum = 0;
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                firstDiagonalSum += matrix[row, row];
+            }
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                secondDiagonalSum += matrix[row, matrixSize - row - 1];
+            }
+            Console.WriteLine(Math.Abs(firstDiagonalSum - secondDiagonalSum));
+        }
+    }
+}
---
+++ C# Advanced/Multidimensional Arrays - Exercise/KnightGame/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace _07._Knight_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            string[,] boardMatrix = new string[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string currentRow = Console.ReadLine();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    boardMatrix[row, col] = currentRow[col].ToString();
+                }
+            }
+
+            int attackableKnightsCounter = 0;
+            int removedKnightsCounter = 0;
+
+            for (int maxAttackPotential = 8; maxAttackPotential > 0; maxAttackPotential--)
+            {
+                for (int row = 0; row < matrixSize; row++)
+                {
+                    for (int col = 0; col < matrixSize; col++)
+                    {
+                        if (boardMatrix[row, col].ToLower() == "k")
+                        {
+                            if (row - 1 >= 0)
+                            {
+                                if (col - 2 >= 0)
+                                {
+                                    if (boardMatrix[row - 1, col - 2].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+
+                                if (col + 2 < matrixSize)
+                                {
+                                    if (boardMatrix[row - 1, col + 2].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+                            }
+
+                            if (row + 1 < matrixSize)
+                            {
+                                if (col - 2 >= 0)
+                                {
+                                    if (boardMatrix[row + 1, col - 2].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+
+                                if (col + 2 < matrixSize)
+                                {
+                                    if (boardMatrix[row + 1, col + 2].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+                            }
+
+                            if (row - 2 >= 0)
+                            {
+                                if (col - 1 >= 0)
+                                {
+                                    if (boardMatrix[row - 2, col - 1].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+
+                                if (col + 1 < matrixSize)
+                                {
+                                    if (boardMatrix[row - 2, col + 1].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+                            }
+
+                            if (row + 2 < matrixSize)
+                            {
+                                if (col - 1 >= 0)
+                                {
+                                    if (boardMatrix[row + 2, col - 1].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+
+                                if (col + 1 < matrixSize)
+                                {
+                                    if (boardMatrix[row + 2, col + 1].ToLower() == "k")
+                                    {
+                                        attackableKnightsCounter++;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (attackableKnightsCounter == maxAttackPotential)
+                        {
+                            boardMatrix[row, col] = "0";
+                            removedKnightsCounter++;
+                        }
+
+                        attackableKnightsCounter = 0;
+                    }
+                }
+            }
+            Console.WriteLine(removedKnightsCounter);
+        }
+    }
+}

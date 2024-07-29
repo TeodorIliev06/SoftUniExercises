ac302a2995b88c6abe1c9d8dccc6fb33cc77f31e
TeodorIliev06 <tedo2505@gmail.com>
2024-07-29 15:12:10 +0300

    Added Generics - Lab

---
+++ C# Advanced/Generics - Lab/GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> test = new EqualityScale<int>(2, 3);
+            Console.WriteLine(test.AreEqual());
+        }
+    }
+}

9bd2a63255652da53f50e8b8bbd220a274372abb
TeodorIliev06 <tedo2505@gmail.com>
2024-07-31 17:14:17 +0300

    Added Iterators and Comparators - lab

---
+++ C# Advanced/Iterators and Comparators - Lab/ComparableBook/BookComparator.cs
@@ -0,0 +1,16 @@
+﻿namespace IteratorsAndComparators;
+
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        int alphabeticalComparison = x.Year.CompareTo(y.Year);
+
+        if (alphabeticalComparison == 0)
+        {
+            return y.Title.CompareTo(x.Title);
+        }
+
+        return alphabeticalComparison;
+    }
+}

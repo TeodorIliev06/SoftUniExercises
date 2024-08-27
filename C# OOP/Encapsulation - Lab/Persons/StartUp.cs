9b7bd148251ea76bae20d678a508d44684a69237
TeodorIliev06 <tedo2505@gmail.com>
2024-08-27 16:10:46 +0300

    Added solutions for encapsulation

---
+++ C# OOP/Encapsulation - Lab/Persons/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonsInfo;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+        List<Person> persons = new();
+
+        for (int i = 0; i < lines; i++)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string firstName = personInfo[0];
+            string lastName = personInfo[1];
+            int age = int.Parse(personInfo[2]);
+
+            persons.Add(new Person(firstName, lastName, age));
+        }
+
+        persons.OrderBy(p => p.FirstName)
+               .ThenBy(p => p.Age)
+               .ToList()
+               .ForEach(p => Console.WriteLine(p));
+    }
+}

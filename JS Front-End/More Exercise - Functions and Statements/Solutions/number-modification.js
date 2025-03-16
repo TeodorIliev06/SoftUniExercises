5548239961c212b41d47db688200b64cf7f96b81
TeodorIliev06 <tedo2505@gmail.com>
2025-03-16 18:39:02 +0200

    Add second batch of js functions exercises

---
+++ JS Front-End/More Exercise - Functions and Statements/Solutions/number-modification.js
@@ -0,0 +1,17 @@
+function modifyNumber(input) {
+    function getDigitAverage(number) {
+        const digits = number.toString().split('');
+        const sum = digits.reduce((acc, digit) => acc + Number(digit), 0);
+        
+        const average = sum / digits.length;
+        return average;
+    }
+
+    while (getDigitAverage(input) < 5) {
+        input = input * 10 + 9;
+    }
+
+    console.log(input);
+}
+
+modifyNumber(5835);

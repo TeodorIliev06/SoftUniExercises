48d1da5fbc68659dd74dbebe5c28a3d1c086886b
TeodorIliev06 <tedo2505@gmail.com>
2025-03-30 09:15:34 +0300

    Add exam structure

---
+++ EF Core/Regular Exam/SocialNetwork_Skeleton/SocialNetwork/DataProcessor/Deserializer.cs
@@ -0,0 +1,41 @@
+﻿using SocialNetwork.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.DataProcessor
+{
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data format.";
+        private const string DuplicatedDataMessage = "Duplicated data.";
+        private const string SuccessfullyImportedMessageEntity = "Successfully imported message (Sent at: {0}, Status: {1})";
+        private const string SuccessfullyImportedPostEntity = "Successfully imported post (Creator {0}, Created at: {1})";
+
+        public static string ImportMessages(SocialNetworkDbContext dbContext, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportPosts(SocialNetworkDbContext dbContext, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsValid(object dto)
+        {
+            ValidationContext validationContext = new ValidationContext(dto);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(dto, validationContext, validationResults, true);
+
+            foreach (ValidationResult validationResult in validationResults)
+            {
+                if (validationResult.ErrorMessage != null)
+                {
+                    string currentMessage = validationResult.ErrorMessage;
+                }
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ EF Core/Regular Exam/SocialNetwork_Skeleton/SocialNetwork/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork.Data;
+
+namespace SocialNetwork.DataProcessor
+{
+    public class Serializer
+    {
+        public static string ExportUsersWithFriendShipsCountAndTheirPosts(SocialNetworkDbContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportConversationsWithMessagesChronologically(SocialNetworkDbContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

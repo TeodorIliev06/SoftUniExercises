6c817a55c9ef07513c293114ce52d696675bc810
TeodorIliev06 <tedo2505@gmail.com>
2025-04-01 19:15:35 +0300

    Add permutation exercises

---
+++ C# Algorithms/Combinatorial Problems - Lab/PermutationsWithRepetitions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PermutationsWithoutRepetitions
+{
+    internal class Program
+    {
+        private static string[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(String.Join(" ", elements));
+                return;
+            }
+
+            //We want to reach the base case for every iteration
+            Permute(index + 1);
+
+            var used = new HashSet<string> { elements[index] };
+
+            for (int i = index + 1; i < elements.Length; i++)
+            {
+                if (!used.Contains(elements[i]))
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+
+                    used.Add(elements[i]);
+                }
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            //(elements[first], elements[second]) = (elements[second], elements[first]);
+
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
---
+++ C# Algorithms/Combinatorial Problems - Lab/PermutationsWithoutRepetitions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PermutationsWithoutRepetitions
+{
+    internal class Program
+    {
+        private static string[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(String.Join(" ", elements));
+                return;
+            }
+
+            //We want to reach the base case for every iteration
+            Permute(index + 1);
+
+            for (int i = index + 1; i < elements.Length; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i);
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            //(elements[first], elements[second]) = (elements[second], elements[first]);
+
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}

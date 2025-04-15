148720354eca18caa113b1db8b301bebcf2dea94
TeodorIliev06 <tedo2505@gmail.com>
2025-04-15 08:25:00 +0300

    Add more backtracking exercises

---
+++ C# Algorithms/Recursion and Combinatorial Problems - Exercise/NestedLoops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NestedLoops
+{
+    internal class Program
+    {
+        private static int[] elements;
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            elements = new int[number];
+            GenerateVectors(0);
+        }
+
+        private static void GenerateVectors(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(String.Join(" ", elements));
+                return;
+            }
+
+            //Going to the bottom then iterating and repeating so as to increment up to N
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                elements[index] = i;
+                GenerateVectors(index + 1);
+            }
+        }
+    }
+}
---
+++ C# Algorithms/Recursion and Combinatorial Problems - Exercise/ReverseArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            ReverseArray(array, 0, array.Length - 1);
+            Console.WriteLine(String.Join(' ', array));
+        }
+
+        private static void ReverseArray(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            (array[start], array[end]) = (array[end], array[start]);
+
+            ReverseArray(array, start + 1, end - 1);
+        }
+    }
+}

0ebdcce473cfc88c9e6094851f2e1a4af93ddeda
TeodorIliev06 <tedo2505@gmail.com>
2025-06-19 22:42:56 +0300

    Extract identity configurations

---
+++ ASP NET Fundamentals/Horizons/Horizons.Web.Infrastructure/IdentityOptionsConfiguratior.cs
@@ -0,0 +1,24 @@
+﻿namespace Horizons.Web.Infrastructure
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+
+    public static class IdentityOptionsConfigurator
+    {
+        public static void Configure(WebApplicationBuilder builder, IdentityOptions cfg)
+        {
+            cfg.SignIn.RequireConfirmedAccount =
+                builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+
+            cfg.Password.RequireDigit =
+                builder.Configuration.GetValue<bool>("Identity:Password:RequireDigits");
+            cfg.Password.RequireNonAlphanumeric =
+                builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
+            cfg.Password.RequireUppercase =
+                builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+            cfg.Password.RequireLowercase =
+                builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+        }
+    }
+}
